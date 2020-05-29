e027838440c5173831d26f79ae75ec240756e31d
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2020-05-29 13:51:17 +0530

    added Radar Loading component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HoverCard3D from './components/3DHoverCard';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Backdrop from './components/Backdrop';
+import RadarLoading from './components/BounceLoading';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route path="/3d_hover_card" component={HoverCard3D}/>
         <Route path="/backdrop" component={Backdrop}/>
+        <Route path="/radarLoader" component={RadarLoading}/>
       </Switch>
     </Router>
     );
---
+++ src/components/BounceLoading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/css/radarLoading.css';
+
+const RadarLoading = () => {
+  return (
+  <div className="pace">
+      <div className="pace-activity"></div>
+  </div>
+  );
+};
+
+export default RadarLoading;
