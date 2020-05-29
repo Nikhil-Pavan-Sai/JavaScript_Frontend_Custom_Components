69a822244a9fd1b019b1fdee2bb53ae375d4fc09
MNikhil0210 <nikhilpavansai.machavaram@zemosolabs.com>
2020-05-29 12:29:52 +0530

    added backdrop component i.e., loading component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import HoverCard3D from './components/3DHoverCard';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Backdrop from './components/Backdrop';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/3d_hover_card" component={HoverCard3D}/>
+        <Route path="/backdrop" component={Backdrop}/>
       </Switch>
     </Router>
     );
---
+++ src/components/Backdrop/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../assets/css/loader.css';
+
+const Backdrop = () => {
+  return <div className="loader"></div>;
+};
+
+export default Backdrop;
