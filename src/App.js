5fd1deb4b69ec6f95ff421de31c5a1cf24a04eeb
Nikhil-Intruder <nikhilpavansai.machavaram@zemosolabs.com>
2020-10-02 12:15:47 +0530

    feat: add Card component using material-ui to component library (#5)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import './App.css';
-import HoverCard3D from './components/3DHoverCard';
+import React from "react";
+import "./App.css";
+import HoverCard3D from "./components/3DHoverCard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Backdrop from './components/Backdrop';
-import RadarLoading from './components/RadarLoading';
+import Backdrop from "./components/Backdrop";
+import RadarLoading from "./components/RadarLoading";
+import CustomCard from "./components/Card";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/3d_hover_card" component={HoverCard3D}/>
-        <Route path="/backdrop" component={Backdrop}/>
-        <Route path="/radarLoader" component={RadarLoading}/>
+        <Route path="/3d_hover_card" component={HoverCard3D} />
+        <Route path="/backdrop" component={Backdrop} />
+        <Route path="/radarLoader" component={RadarLoading} />
+        <Route path="/card" component={CustomCard} />
       </Switch>
     </Router>
-    );
+  );
 }
 
 export default App;
